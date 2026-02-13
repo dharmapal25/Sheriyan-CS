b6ecb3acbf98fda7f78a0450672a539e6993385f
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-02-13 13:23:15 +0530

    Web OS UI drag-drap window tabs

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css"
+import Windows from './Windows'
+const Home = () => {
+  return (
+    <div>
+
+        <div className="desktop-view">
+
+            <Windows/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
